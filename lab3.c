692922acaf1b1472d98492afd849f5c41b5de4e0
MaryLukyanova <bobbkrist@gmail.com>
2016-10-22 16:05:06 +0400

    Add files via upload

---
+++ lab3.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+int main() {
+   int fd = open("lala.txt", O_WRONLY|O_CREAT, 0777);
+   if (fd==-1) { perror("open"); } // errno, #include <errno.h>
+   fprintf(stderr, "File is opened, fd=%d\n", fd);
+
+   int ret = write(fd, "Lol\n", 4);
+   fprintf(stderr, "Write is finished, %d bytes written\n", ret);
+   return 0;
+}
+
